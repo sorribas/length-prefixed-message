2796553458f57c43db4fecb59d6722886ba96cd7
Eduardo Sorribas <eduardo@sorribas.org>
2014-08-16 18:51:15 +0200

    Add chunked stream read test.

--- test.js
+++ test.js
@@ -2,11 +2,21 @@ var fs = require('fs');
 var test = require('tape');
 var concat = require('concat-stream');
 var lpm = require('./');
+var choppa = require('choppa');
 var varint = require('varint');
 
 test('.read', function(t) {
   lpm.read(fs.createReadStream('./fixtures'), function(buf) {
     t.equal(buf.toString(), 'When Gregor Samsa woke up one morning from unsettling dreams, he found himself changed \n');
+    t.end();
+  });
+});
+
+test('.read with chunked stream', function(t) {
+  var chp = choppa();
+  fs.createReadStream('./fixtures2').pipe(chp);
+  lpm.read(chp, function(buf) {
+    t.equal(buf.toString().substr(0, 33), 'As Gregor Samsa awoke one morning');
     t.end();
   });
 });
